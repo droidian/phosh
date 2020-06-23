7c050922e1b6d9d295c066cdfcaee46dcf9641ec
Guido Günther <agx@sigxcpu.org>
2020-06-23 14:08:53 +0200

    rotateinfo: Display either rotation lock or orientation
    We listen for the shell's rotation-manager property first
    and if that show up connect to `orientation-locked` to
    update the icon status.
    
    If no orientation manager is available use the current
    portrait/landscape mode.
    
    Add helpers that allow to switch between the two modes.
    
    Signed-off-by: Guido Günther <guido.gunther@puri.sm>

--- src/rotateinfo.c
+++ src/rotateinfo.c
@@ -15,22 +15,25 @@
 
 /**
  * SECTION:rotateinfo
- * @short_description: A widget to display the rotate status
+ * @short_description: A widget to display the rotate lock status
  * @Title: PhoshRotateInfo
  *
- * Rotate Info widget
+ * A #PhoshStatusIcon to display the rotation lock status.
+ * It can either display whether a rotation lock is currently active or
+ * if the output is in portrait/landscape mode.
  */
 
 typedef struct _PhoshRotateInfo {
-  PhoshStatusIcon parent;
+  PhoshStatusIcon     parent;
+
+  PhoshRotateInfoMode mode;
 } PhoshRotateInfo;
 
 
 G_DEFINE_TYPE (PhoshRotateInfo, phosh_rotate_info, PHOSH_TYPE_STATUS_ICON)
 
-
 static void
-set_state (PhoshRotateInfo *self)
+set_toggle_state (PhoshRotateInfo *self)
 {
   PhoshShell *shell = phosh_shell_get_default ();
   PhoshMonitor *monitor = phosh_shell_get_primary_monitor (shell);
@@ -55,6 +58,9 @@ set_state (PhoshRotateInfo *self)
     portrait = TRUE;
   }
 
+  if (self->mode != PHOSH_ROTATE_INFO_MODE_TOGGLE)
+    return;
+
   /* If we have a landscape monitor (tv, laptop) flip the rotation */
   monitor_is_landscape = ((double)monitor->width / (double)monitor->height) > 1.0;
   portrait = monitor_is_landscape ? !portrait : portrait;
@@ -69,39 +75,144 @@ set_state (PhoshRotateInfo *self)
   }
 }
 
-
-static void
-phosh_rotate_info_finalize (GObject *object)
+static gboolean
+binding_set_lock_icon (GBinding        *binding,
+                       const GValue    *from_value,
+                       GValue          *to_value,
+                       PhoshRotateInfo *self)
 {
-  PhoshRotateInfo *self = PHOSH_ROTATE_INFO(object);
+  gboolean locked = g_value_get_boolean (from_value);
+  const char *icon_name;
 
-  g_signal_handlers_disconnect_by_data (phosh_shell_get_default (), self);
+  icon_name = locked ? "rotation-locked-symbolic" : "rotation-allowed-symbolic";
+  g_value_set_string (to_value, icon_name);
 
-  G_OBJECT_CLASS (phosh_rotate_info_parent_class)->finalize (object);
+  return TRUE;
+}
+
+static gboolean
+binding_set_lock_info (GBinding        *binding,
+                       const GValue    *from_value,
+                       GValue          *to_value,
+                       PhoshRotateInfo *self)
+
+{
+  gboolean locked = g_value_get_boolean (from_value);
+  const gchar *info;
+
+  if (self->mode != PHOSH_ROTATE_INFO_MODE_LOCK)
+    return TRUE;
+
+  /* Translators: Automatic screen orientation is either on (enabled) or off (locked/disabled) */
+  info = locked ? _("Off") : _("On");
+  g_value_set_string (to_value, info);
+
+  return TRUE;
 }
 
 
 static void
 phosh_rotate_info_class_init (PhoshRotateInfoClass *klass)
 {
-  GObjectClass *object_class = G_OBJECT_CLASS (klass);
-  object_class->finalize = phosh_rotate_info_finalize;
 }
 
 
 static void
-phosh_rotate_info_init (PhoshRotateInfo *self)
+on_rotation_manager_changed (PhoshRotateInfo *self, GParamSpec *pspec, PhoshShell *shell)
 {
-  g_signal_connect_swapped (phosh_shell_get_default (),
-                            "notify::transform",
-                            G_CALLBACK (set_state),
-                            self);
-  set_state (self);
+  PhoshRotationManager *rotation_manager;
+
+  g_return_if_fail (PHOSH_IS_ROTATE_INFO (self));
+  g_return_if_fail (PHOSH_SHELL (shell));
+
+  rotation_manager = phosh_shell_get_rotation_manager (shell);
+  if (!rotation_manager) {
+    phosh_rotate_info_set_mode (self, PHOSH_ROTATE_INFO_MODE_TOGGLE);
+    return;
+  }
+
+  g_object_bind_property_full (rotation_manager,
+                               "orientation-locked",
+                               self,
+                               "icon-name",
+                               G_BINDING_SYNC_CREATE
+                               | G_BINDING_DEFAULT,
+                               (GBindingTransformFunc)binding_set_lock_icon,
+                               NULL,
+                               self,
+                               NULL);
+  g_object_bind_property_full (rotation_manager,
+                               "orientation-locked",
+                               self,
+                               "info",
+                               G_BINDING_SYNC_CREATE
+                               | G_BINDING_DEFAULT,
+                               (GBindingTransformFunc)binding_set_lock_info,
+                               NULL,
+                               self,
+                               NULL);
+  phosh_rotate_info_set_mode (self, PHOSH_ROTATE_INFO_MODE_LOCK);
 }
 
+static void
+phosh_rotate_info_init (PhoshRotateInfo *self)
+{
+  self->mode = PHOSH_ROTATE_INFO_MODE_TOGGLE;
+
+  g_signal_connect_object (phosh_shell_get_default (),
+                           "notify::rotation",
+                           G_CALLBACK (set_toggle_state),
+                           self,
+                           G_CONNECT_SWAPPED);
+  set_toggle_state (self);
+
+  /* Rotation manager might not be there when iio-sensor-proxy is missing
+     so only connect to it once valid */
+  g_signal_connect_object (phosh_shell_get_default (),
+                           "notify::rotation-manager",
+                           G_CALLBACK (on_rotation_manager_changed),
+                           self,
+                           G_CONNECT_SWAPPED);
+}
 
 GtkWidget *
 phosh_rotate_info_new (void)
 {
   return g_object_new (PHOSH_TYPE_ROTATE_INFO, NULL);
+}
+
+PhoshRotateInfoMode
+phosh_rotate_info_get_mode (PhoshRotateInfo *self)
+{
+  g_return_val_if_fail (PHOSH_IS_ROTATE_INFO (self), PHOSH_ROTATE_INFO_MODE_LOCK);
+
+  return self->mode;
+}
+
+void
+phosh_rotate_info_set_mode (PhoshRotateInfo *self, PhoshRotateInfoMode mode)
+{
+  PhoshRotationManager *rotation_manager;
+  PhoshShell *shell  = phosh_shell_get_default ();
+
+  g_return_if_fail (PHOSH_IS_ROTATE_INFO (self));
+
+  if (mode == self->mode)
+    return;
+
+  self->mode = mode;
+
+  g_debug ("Setting mode: %d", mode);
+  switch (mode) {
+  case PHOSH_ROTATE_INFO_MODE_TOGGLE:
+    set_toggle_state (self);
+    break;
+  case PHOSH_ROTATE_INFO_MODE_LOCK:
+    rotation_manager = phosh_shell_get_rotation_manager (shell);
+    if (rotation_manager)
+      g_object_notify (G_OBJECT (rotation_manager), "orientation-locked");
+    break;
+  default:
+    g_assert_not_reached ();
+  }
 }
