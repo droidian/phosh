610a52995d9bf2c30e278db89ef7beab124498b4
Eugenio Paolantonio (g7) <me@medesimo.eu>
2020-09-13 22:23:08 +0200

    util: implement phosh_convert_buffer
    This function can be used to ensure an eventual BGR format can be
    converted to RGB so that cairo is happy.
    
    Signed-off-by: Eugenio Paolantonio (g7) <me@medesimo.eu>

--- src/util.h
+++ src/util.h
@@ -6,7 +6,9 @@
 #pragma once
 
 #include <gtk/gtk.h>
+#include <wayland-client-protocol.h>
 
 void phosh_cp_widget_destroy (void *widget);
 gchar* phosh_fix_app_id (const gchar* app_id);
 void phosh_clear_handler (gulong *handler, gpointer object);
+void phosh_convert_buffer (void *data, enum wl_shm_format format, guint width, guint height, guint stride);
