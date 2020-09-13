5488c009b7a359dd0e8a0308d6703c6a826b837b
Eugenio Paolantonio (g7) <me@medesimo.eu>
2020-09-13 22:23:08 +0200

    util: implement phosh_convert_buffer
    This function can be used to ensure an eventual BGR format can be
    converted to RGB so that cairo is happy.
    
    Signed-off-by: Eugenio Paolantonio (g7) <me@medesimo.eu>

--- src/util.c
+++ src/util.c
@@ -18,6 +18,7 @@
 
 #include "util.h"
 #include <gtk/gtk.h>
+#include <wayland-client-protocol.h>
 
 #include <locale.h>
 #include <glib/gi18n.h>
@@ -193,6 +194,45 @@ phosh_find_systemd_session (char **session_id)
   }
 
   return FALSE;
+}
+
+/**
+ * phosh_convert_buffer:
+ * @data: the buffer data to modify
+ * @format: the current pixel format
+ * @width: image width
+ * @height: image height
+ * @stride: image stride
+ *
+ * Converts the buffer to ARGB format so that
+ * is suitable for usage in Cairo.
+ * If the buffer is already ARGB (or the conversion
+ * is not implemented), nothing happens.
+*/
+void
+phosh_convert_buffer (void *data, enum wl_shm_format format, guint width, guint height, guint stride)
+{
+  switch (format) {
+  case WL_SHM_FORMAT_ABGR8888:
+  case WL_SHM_FORMAT_XBGR8888:
+    ;
+    guint8 *_data = (guint8 *)data;
+    for (int i = 0; i < height; i++) {
+      for (int j = 0; j < width; j++) {
+          guint32 *px = (guint32 *)(_data + i * stride + j * 4);
+          guint8 r, g, b, a;
+
+          a = (*px >> 24) & 0xFF;
+          b = (*px >> 16) & 0xFF;
+          g = (*px >> 8) & 0xFF;
+          r = *px & 0xFF;
+          *px = (a << 24) | (r << 16) | (g << 8) | b;
+      }
+    }
+    break;
+  default:
+    break;
+  }
 }
 
 /**
