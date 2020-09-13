4a406b056fa2cc8c3ca4202bb0bf5badb5b6cddc
Eugenio Paolantonio (g7) <me@medesimo.eu>
2020-09-13 22:28:22 +0200

    activity: phosh_activity_set_thumbnail: convert buffer data to ensure it's in ARGB format
    Signed-off-by: Eugenio Paolantonio (g7) <me@medesimo.eu>

--- src/activity.c
+++ src/activity.c
@@ -738,6 +738,7 @@ phosh_activity_set_thumbnail (PhoshActivity *self, PhoshThumbnail *thumbnail)
   gpointer data;
   guint w, width, height, stride, margin;
   float scale;
+  enum wl_shm_format format;
 
   g_return_if_fail (PHOSH_IS_ACTIVITY (self));
   priv = phosh_activity_get_instance_private (self);
@@ -747,6 +748,9 @@ phosh_activity_set_thumbnail (PhoshActivity *self, PhoshThumbnail *thumbnail)
 
   data = phosh_thumbnail_get_image (thumbnail);
   phosh_thumbnail_get_size (thumbnail, &width, &height, &stride);
+  phosh_thumbnail_get_format (thumbnail, &format);
+
+  phosh_convert_buffer (data, format, width, height, stride);
 
   priv->surface = cairo_image_surface_create_for_data (
       data, CAIRO_FORMAT_ARGB32, width, height, stride);
