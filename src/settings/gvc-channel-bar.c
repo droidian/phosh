a5c71bafc7ed421a1ded7de9a3be42c8860915b1
Eugenio Paolantonio (g7) <me@medesimo.eu>
2021-09-05 13:09:39 +0200

    settings: gvc-channel-bar: allow to create a bar with a custom icon via the new gvc_channel_bar_new_with_icon() function
    Signed-off-by: Eugenio Paolantonio (g7) <me@medesimo.eu>

--- src/settings/gvc-channel-bar.c
+++ src/settings/gvc-channel-bar.c
@@ -391,3 +391,15 @@ gvc_channel_bar_get_volume (GvcChannelBar *self)
 
   return gtk_adjustment_get_value (self->adjustment);
 }
+
+
+GtkWidget *
+gvc_channel_bar_new_with_icon (const char *icon_name)
+{
+  GObject *self;
+  self = g_object_new (GVC_TYPE_CHANNEL_BAR,
+                       "orientation", GTK_ORIENTATION_HORIZONTAL,
+                       "icon-name", icon_name,
+                       NULL);
+  return GTK_WIDGET (self);
+}
