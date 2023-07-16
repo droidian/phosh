35c1d57fd90ada6166361b09d9ff7837d3ff2f7c
mathew-dennis <78794223+mathew-dennis@users.noreply.github.com>
2023-07-16 16:13:52 +0530

    test 5

--- src/home.c
+++ src/home.c
@@ -156,7 +156,7 @@ update_drag_handle (PhoshHome *self, gboolean commit)
   PhoshDragSurfaceState drag_state = phosh_drag_surface_get_drag_state (PHOSH_DRAG_SURFACE (self));
 
   /* reset powerbar gesture when dragging */
-  if (gtk_gesture_get_sequence_state ((self->osk_toggle_long_press), sequence) == GTK_EVENT_SEQUENCE_DENIED) {
+  if (gtk_gesture_get_sequence_state ((self->osk_toggle_long_press), GdkEventSequence *sequence) == GTK_EVENT_SEQUENCE_DENIED) {
     gtk_gesture_set_state ((self->osk_toggle_long_press), GTK_EVENT_SEQUENCE_NONE);
   } else {
     gtk_gesture_set_state ((self->osk_toggle_long_press), GTK_EVENT_SEQUENCE_DENIED);
