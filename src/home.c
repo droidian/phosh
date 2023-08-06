774f1a967ab57c1e8fc59a5b1a593a02f196ca8a
mathew-dennis <78794223+mathew-dennis@users.noreply.github.com>
2023-08-06 16:43:33 +0530

    test

--- src/home.c
+++ src/home.c
@@ -172,6 +172,9 @@ update_drag_handle (PhoshHome *self, gboolean commit)
     g_warning ("enabling long press");
   }
  */
+ 
+  gtk_gesture_set_state ((self->osk_toggle_long_press), GTK_EVENT_SEQUENCE_NONE);
+
   /* Update the handle's arrow and dragability */
   if (phosh_overview_has_running_activities (PHOSH_OVERVIEW (self->overview)) == FALSE &&
     self->state == PHOSH_HOME_STATE_UNFOLDED && drag_state != PHOSH_DRAG_SURFACE_STATE_DRAGGED) {
