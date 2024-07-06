b38a0a3219ea189a78c16a83b49d272ae4190243
Eugenio Paolantonio (g7) <me@medesimo.eu>
2024-07-06 14:45:42 +0200

    Reapply "wayland: Drop kde idle protocol"
    This reverts commit fac5d156b2f905279b85eb25a38412ce8bfc3068.

--- src/phosh-wayland.c
+++ src/phosh-wayland.c
@@ -41,7 +41,6 @@ struct _PhoshWayland {
   GObject parent;
 
   struct ext_idle_notifier_v1 *ext_idle_notifier_v1;
-  struct org_kde_kwin_idle *idle_manager;
   struct phosh_private *phosh_private;
   uint32_t phosh_private_version;
   struct zwp_virtual_keyboard_manager_v1 *zwp_virtual_keyboard_manager_v1;
@@ -109,12 +108,6 @@ registry_handle_global (void *data,
     g_debug ("Got new output %p", output);
     g_hash_table_insert (self->wl_outputs, GINT_TO_POINTER (name), output);
     g_object_notify_by_pspec (G_OBJECT (self), props[PHOSH_WAYLAND_PROP_WL_OUTPUTS]);
-  } else if (!strcmp (interface, "org_kde_kwin_idle")) {
-    self->idle_manager = wl_registry_bind (
-      registry,
-      name,
-      &org_kde_kwin_idle_interface,
-      1);
   } else if (!strcmp(interface, "wl_seat")) {
     self->wl_seat = wl_registry_bind(
       registry, name, &wl_seat_interface,
@@ -300,7 +293,7 @@ phosh_wayland_constructed (GObject *object)
   /* Wait until we have been notified about the wayland globals we require */
   phosh_wayland_roundtrip (self);
   num_outputs = g_hash_table_size(self->wl_outputs);
-  if (!num_outputs || !self->layer_shell || !self->idle_manager ||
+  if (!num_outputs || !self->layer_shell || !self->ext_idle_notifier_v1 ||
       !self->xdg_wm_base ||
       !self->zxdg_output_manager_v1 ||
       !self->zwlr_output_power_manager_v1 ||
@@ -313,7 +306,7 @@ phosh_wayland_constructed (GObject *object)
              "zwlr_output_power_manager_v1: %p, "
              "zphoc_layer_shell_effects_v1: %p"
              "\n",
-             num_outputs, self->layer_shell, self->idle_manager,
+             num_outputs, self->layer_shell, self->ext_idle_notifier_v1,
              self->xdg_wm_base,
              self->zxdg_output_manager_v1,
              self->zwlr_output_manager_v1,
@@ -340,7 +333,6 @@ phosh_wayland_dispose (GObject *object)
   PhoshWayland *self = PHOSH_WAYLAND (object);
 
   g_clear_pointer (&self->ext_idle_notifier_v1, ext_idle_notifier_v1_destroy);
-  g_clear_pointer (&self->idle_manager, org_kde_kwin_idle_destroy);
   g_clear_pointer (&self->layer_shell, &zwlr_layer_shell_v1_destroy);
   g_clear_pointer (&self->phosh_private, phosh_private_destroy);
   g_clear_pointer (&self->registry, wl_registry_destroy);
@@ -471,15 +463,6 @@ phosh_wayland_get_ext_idle_notifier_v1 (PhoshWayland *self)
   g_return_val_if_fail (PHOSH_IS_WAYLAND (self), NULL);
 
   return self->ext_idle_notifier_v1;
-}
-
-
-struct org_kde_kwin_idle*
-phosh_wayland_get_org_kde_kwin_idle (PhoshWayland *self)
-{
-  g_return_val_if_fail (PHOSH_IS_WAYLAND (self), NULL);
-
-  return self->idle_manager;
 }
 
 
