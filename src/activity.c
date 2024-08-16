5a78eb6709977331ce5c7de2312288173cc47c55
Guido Günther <agx@sigxcpu.org>
2024-08-16 09:53:00 +0200

    activity: Use simpler phosh_util_toggle_style_class
    Signed-off-by: Guido Günther <agx@sigxcpu.org>

--- src/activity.c
+++ src/activity.c
@@ -699,7 +699,7 @@ phosh_activity_set_thumbnail (PhoshActivity *self, PhoshThumbnail *thumbnail)
       data, CAIRO_FORMAT_ARGB32, width, height, stride);
   priv->thumbnail = thumbnail;
 
-  gtk_style_context_remove_class (gtk_widget_get_style_context (GTK_WIDGET (self)), "phosh-empty");
+  phosh_util_toggle_style_class (GTK_WIDGET (self), "phosh-empty", FALSE);
 
   /* Make sure the close button is over the thumbnail */
   w = gtk_widget_get_allocated_width (GTK_WIDGET (self));
