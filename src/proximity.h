ac03c400fcf1b0b7ba6f3187230068ef06850b45
Cédric Bellegarde <cedric.bellegarde@adishatz.org>
2025-08-04 13:34:49 +0200

    shell: Blank the screen instead of using a fader for calls
    When accepting a call, if the proximity sensor returns a "near" state,
    immediately blank the screen instead of using a fader.

--- src/proximity.h
+++ src/proximity.h
@@ -19,7 +19,7 @@ G_DECLARE_FINAL_TYPE (PhoshProximity, phosh_proximity, PHOSH, PROXIMITY, GObject
 
 PhoshProximity *phosh_proximity_new (PhoshSensorProxyManager *sensor_proxy_manager,
                                      PhoshCallsManager       *calls_manager);
-gboolean        phosh_proximity_has_fader (PhoshProximity *sensor_proxy_manager);
 gboolean        phosh_proximity_sensor_enabled (PhoshProximity *sensor_proxy_manager);
+gboolean        phosh_proximity_near (PhoshProximity *sensor_proxy_manager);
 
 G_END_DECLS
